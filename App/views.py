98d85bfa504e4beef605dc3e9cb7b7cdb8ed8991
Emmanuel Oluyemi <e8060097@gmailcom>
2024-12-27 02:52:05 +0000

    Save local changes before pulling updates

--- App/views.py
+++ App/views.py
@@ -12,14 +12,14 @@ from django.conf import settings
 def index(request):
     if request.method == 'POST':
         OwnerUsername =  request.POST['OwnerUsername']
-        
+
         if Users.objects.filter(OwnerUsername=OwnerUsername).exists():
             return redirect('view_secret_link/'+OwnerUsername)
         else:
             user = Users.objects.create(OwnerUsername=OwnerUsername)
             user.save()
             return redirect('view_secret_link/'+OwnerUsername)
-        
+
     return render(request, 'index.html')
 
 def register(request):
@@ -28,13 +28,13 @@ def register(request):
         email = request.POST['email']
         password = request.POST['password']
         password2 = request.POST['password2']
-        
+
         #Strip all white spaces
         username = username.strip()
         email = email.strip()
         password = password.strip()
         password2 = password2.strip()
-        
+
         if password == password2:
             if User.objects.filter(email=email).exists():
                 messages.info(request, 'Email Already Exists')
@@ -46,21 +46,22 @@ def register(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
                 send_mail(
-                    """Welcome to WhisperZone!,
-                    Hi there! 
+                    subject="Welcome to WhisperZone!",  # Add the subject
+                    message="""Hi there!
 
                     WhisperZone allows you to send and receive anonymous messages using your unique link.
-                    Welcome to WhisperZone! We're thrilled to have you here. 
+                    Welcome to WhisperZone! We're thrilled to have you here.
 
-                    Get started by sharing your link with your friends: 
-                    
+                    Get started by sharing your link with your friends:
+
                     Happy messaging,
                     The WhisperZone Team
                     """,
-                    settings.EMAIL_HOST_USER,    # Sender's email address
-                    [email],                     # Recipient's email address in a list
+                    from_email=settings.EMAIL_HOST_USER,  # Sender's email address
+                    recipient_list=[email],  # List of recipients
                     fail_silently=False
                 )
+
                 return redirect('login')
         else:
             messages.info(request, 'Passwords does not match')
@@ -70,10 +71,10 @@ def login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        
+
         username = username.strip()
         password = password.strip()
-        
+
         user = auth.authenticate(username=username, password=password)
         if user is not None:
             auth.login(request, user)
@@ -100,7 +101,7 @@ def send_message(request, pk):
 
             AnonymousMessage.objects.create(OwnerUsername=OwnerUsername, message=message)
 
-            print('Message saved successfully!')
+            messages.info(request,'Message sent successfully!')
             return redirect('/send_message/' + pk)
         else:
             return render(request, 'send_message.html')
